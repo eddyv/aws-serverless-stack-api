b9a71ecfb46e693e029919782a894c535d7a9ceb
eddyv <vaismanedward@gmail.com>
2021-05-09 13:49:24 -0400

    adding s3 bucket to our infra

---
+++ infrastructure/lib/S3Stack.js
@@ -0,0 +1,28 @@
+import cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as sst from "@serverless-stack/resources";
+
+export default class S3Stack extends sst.Stack {
+  // public reference to s3 bucket
+  bucket;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    this.bucket = new s3.Bucket(this, "Uploads", {
+      // Allow client side access to the bucket from a different domain
+      cors: [
+        {
+          maxAge: 3000,
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"],
+          allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+        },
+      ],
+    });
+
+    new cdk.CfnOutput(this, "AttachmentsBucketName", {
+      value: this.bucket.bucketName,
+    });
+  }
+}
